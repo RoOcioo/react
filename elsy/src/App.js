3e9e377523c3d1c830dad19b9a0d5dd249c2eba4
Antoine <bonjour@antoine.cool>
2021-04-07 15:52:43 +0200

    bootstrap import

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import "/https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css";
+import Containers from "react-bootstrap/containers";
 
 
 const tempMin=`-20`
