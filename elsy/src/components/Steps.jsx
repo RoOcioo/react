4431fda59f560b7962441541baeb8be66f4faebb
Antoine <bonjour@antoine.cool>
2021-04-07 16:44:35 +0200

    partie 3

---
+++ elsy/src/components/Steps.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+ 
+class Steps extends React.Component {
+
+
+  render() {
+      <div className= "box col-sm-3 col-6">
+          
+      </div>
+    return (
+			<div>
+				
+			</div>
+		)
+  };
+}
+
+export default Steps;
