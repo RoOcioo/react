6489c1ab723224496195479a2a35dad50c461e84
Antoine <bonjour@antoine.cool>
2021-04-07 14:57:47 +0200

    first test react

---
+++ elsy/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './App.css';
+import "/https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css";
+
+
+const tempMin=`-20`
+const tempMax= `40`
+const heartMin=`80`
+const heartMax=`180`
+const stepsMin= `0`
+const stepsMax= `50000`
+
+
+class App extends React.Component {
+  render() {
+
+    return (
+      <div className= "container-fluid"> 
+     
+      </div>
+    );
+  }
+}
+
+export default App;
