4c2cd7d6641ed149b9d105c29444f36e06ae113c
Antoine <bonjour@antoine.cool>
2021-04-12 17:36:47 +0200

    warning message

--- elsy/src/App.js
+++ elsy/src/App.js
@@ -6,38 +6,50 @@ import './styles/global.css';
 
 
 
-const tempMin=`-20`
-const tempMax= `40`
-const heartMin=`80`
-const heartMax=`180`
-const stepsMin= `0`
-const stepsMax= `50000`
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepsMin = 0
+const stepsMax = 50000
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000
+    };
+  }
+ 
+    onHeartChange(value) {
+      this.state.heart({
+        value: "150"
+      });
+    }
+
+
   render() {
 
     return (
-      <div className= "container-fluid"> 
-      <div className= "row">
-        <Box />
-        
+      <div className="container-fluid">
+        <div className="row">
 
-        <Box icon= "local_drink"color= "#3A85FF"value= {1.5}unit="L"/> 
-        <Box icon= "directions_walk" value= {3000} unit="steps" color="black" />
-        <Box icon="wb_sunny" value={-10} unit="°C" color="yellow" />
-        <Box icon="favorite"value={120}unit= "bpm" color="red"/>
-        
 
-            
-      
-    
-        <p>
-        gitHeart : 80
-        Temperature : -20
-        Steps : 0
-        </p>
-      </div>
+          <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" />
+          <Box icon="directions_walk" value={3000} unit="steps" color="black" />
+          <Box icon="wb_sunny" value={-10} unit="°C" color="yellow" />
+          <Box icon="favorite" value={120} unit="bpm" color="red" />
+
+
+          <p>Heart : {heartMin} </p>
+          <p>Temperature : {tempMin} </p>
+          <p>Steps : {stepsMin}
+          </p>
+        </div>
       </div>
     );
   }
--- elsy/src/components/Box.jsx
+++ elsy/src/components/Box.jsx
@@ -2,28 +2,34 @@ import React from "react";
 
  
 class Box extends React.Component {
-constructor(){
-  super(props)
-}
 
   render() {
+    if (this.props.icon !== "local_drink") {
+      return (
+        <input type="range" min={this.props.stepsMin} 
+        max={this.props.stepsMax} value={this.props.value} onInput/>)
+        
       
-     
+       } else {
+      
     return (
         <div className= "box col-sm-3 col-6">
         <span className="material-icons"
         style= {{
-          fontSize: '100',
+          fontSize: '100px',
           color: this.props.color,
         }}
         >{this.props.icon}
 </span>
-<p>{this.props.value}: {this.props.unit}</p>
+<p>{this.props.value}{this.props.unit}</p>
+
+
         </div>
-				
+      	
 			
-		)
-  };
+		);
+  }
+  }
 }
 
 export default Box;
