f5d65a0555b42dccb75d0aa6d5b0244aa10fb676
Antoine <bonjour@antoine.cool>
2021-04-15 17:33:06 +0200

    lot of problems con regex

--- chapitre_04/form/src/App.js
+++ chapitre_04/form/src/App.js
@@ -14,36 +14,91 @@ class App extends React.Component {
             motdePasse: '',
             submit: false
         }
+   
+    this.funcEmail = this.funcEmail.bind(this);
+    this.funcmotdePasse = this.funcmotdePasse.bind(this);
+    this.funcSubmit = this.funcSubmit.bind(this);
+  }
+
+
+    funcEmail(e) {
+      this.setState({
+        eMail: e.target.value
+      })
+    }
+    funcmotdePasse(e) {
+      this.setState({
+        motdePasse: e.target.value
+      })
+    }
+    funcSubmit() {
+      this.setState({
+        submit: true
+      })
     }
 
+
+
+
       rendereMail() {
-        if (this.state.userName) { 
-            return (
-                <div>
-                    <input>
-                    <input>
+        if ( /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/gm (this.state.eMail))
+           {
+              return (
+                <div className="form-floating mb-3">
+                    <label for= "exampleEmail" class="form-label">Email adress</label>
+                    <input placeholder= "Enter email" onInput= {(e) => this.funcEmail(e)} className= "form-control is-valid" id= "floatingInput"/>
                 </div>
-            );
+            )
         } else { 
             return (
-                <input>
-                   
-                <input>
-            );
+              <div class= "form-floating mb-3">
+                <label for= "exampleEmail" class="form-label">Email adress</label>
+                <input placeholder= "Enter email" onInput= {(e) => this.funcEmail(e)} className= "form-control is-invalid" id= "floatingInput"/>
+
+                </div>
+            ) 
         }
     }
-    
+      rendermotdePasse() {
+        if ( /^(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{6,}$/gm(this.state.motdePasse)) {
+          return (
+            <div class="form-floating">
+            <label for="floatingPassword">Password</label>
+          <input placeholder="Password" onInput= {(e) => this.funcmotdePasse(e) } className="form-control is-valid" id="floatingPassword" />
+          
+        </div>
+          )
+        } else {
+          return (
+            <div class="form-floating">
+            <label for="floatingPassword">Password</label>
+          <input placeholder="Password" onInput= {(e) => this.funcmotdePasse(e)} className="form-control is-invalid" id="floatingPassword" />
+          </div>
+          )
+        }
+
+      }
+    renderSubmit() {
+      if (/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/gm(this.state.eMail) && /^(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{6,}$/gm (this.state.motdePasse))
+       {
+        return <button onClick={this.funcSubmit} type= "submit" class= "btn btn-primary">Submit</button>
+      } else {
+        return <button type = "submit" class= "btn btn-primary" disabled>Submit</button>
+      }
+    }
+
+
     render() {
-        return (
-            <div>
-                {this.renderNavigation()}
-                {this.renderContent()}
-            </div>
-        );
+      if (this.state.submit === false) {
+
+    return (       
     
-      <div className="text-center">
+      <div className="d-flex flex-column align-items-center">
       
          <h1 className="h3 mb-3 fw-normal">Login</h1>
+
+         {this.rendereMail()}
+         {this.rendermotdePasse()}
 
         <div class="form-floating mb-3">
           <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
@@ -53,12 +108,15 @@ class App extends React.Component {
           <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
           <label for="floatingPassword">Password</label>
         </div>
-
         <div className="checkbox mb-3">
       <label>
         <input type="checkbox" value="remember-me" /> Remember me
       </label>
     </div>
+
+        {this.renderSubmit()}
+
+
     <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
 
         
@@ -68,6 +126,8 @@ class App extends React.Component {
 
 
   }
+}
+}
 
 
 export default App;
