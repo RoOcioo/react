67227481e1ccc74df4ba5e703a7fda5bb29c8361
Antoine <bonjour@antoine.cool>
2021-04-16 18:01:07 +0200

    no se guardan los productos

--- chapitre_04/bakery/src/App.js
+++ chapitre_04/bakery/src/App.js
@@ -21,6 +21,7 @@ class App extends React.Component {
     this.selectAdd = this.selectAdd.bind(this);
     this.selectList = this.selectList.bind(this);
     this.selectPay = this.selectPay.bind(this);
+    this.addItem = this.addItem.bind(this);
   }
 
   selectAdd() {
@@ -31,29 +32,79 @@ class App extends React.Component {
 
   selectList() {
     this.setState({ 
-      activeTab:
-       'list' 
+      activeTab: 'list' 
       });
   }
 
   selectPay() {
-    this.setState({ activeTab:
-       'pay' 
+    this.setState({
+       activeTab:'pay' 
       });
   }
+
+  addItem(leNom, lePrix) {
+    let leItem = {
+      name: leNom,
+      price: lePrix
+    };
+    let updateItems = this.state.items;
+
+    updateItems.push(leItem);
+
+    this.setState({ 
+      items: updateItems 
+    });
+    this.selectList()
+  }
+
+
+  popItem() {
+    let arrayItems = this.state.items;
+
+    arrayItems.pop()
+    
+    this.setState({
+    items: arrayItems
+  });
+  }
+
+
+  renderAdd() {
+    if (this.state.activeTab === "add") {
+      return (
+        <div>
+          <Add addItem={this.addItem}/>
+        </div>
+      )
+    } else if (this.state.activeTab === "list") {
+      return (
+        <div>
+          <List popItem={this.popItem} items={this.state.items} />
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <Pay />
+        </div>
+      )
+    }
+
+  }
+
 
       render () {
         return(
           <div className="container d-flex justify-content-center">
           <div className="d-flex flex-column">
   
-            <h1 className="h1" style={{textAlign: 'center'}}>Bakery</h1>
+            <h1 className="h1">Bakery</h1>
             <div className="d-flex flex-row justify-content-start">
               <Button isSelected={this.state.activeTab} onClick={this.selectAdd} >Add</Button>
               <Button isSelected={this.state.activeTab} onClick={this.selectList} >List</Button>
               <Button isSelected={this.state.activeTab} onClick={this.selectPay} >Pay</Button>
             </div>
-  
+            {this.renderAdd()}
           
           </div>
         </div>
--- chapitre_04/bakery/src/composants/Add.jsx
+++ chapitre_04/bakery/src/composants/Add.jsx
@@ -10,6 +10,21 @@ class Add extends React.Component {
             productName: "",
             price: 1
         }
+    
+    this.updateProductName = this.updateProductName.bind(this);
+    this.updatePrice = this.updatePrice.bind(this);
+    }
+
+    updateProductName(e) {
+        this.setState({ 
+        productName: e.target.value 
+    });
+    }
+
+    updatePrice(e) {
+        this.setState({
+         price: e.target.value
+     });
     }
 
 
@@ -19,11 +34,17 @@ class Add extends React.Component {
                 <div className="d-flex flex-column align-items-center">
                 <div className="mb-3" >
                     <div className="input-group mt-3 d-flex align-self-center">
-                        <input type="text" placeholder="Item" onChange={this.updateProductName} class="form-control" aria-label="Item" aria-describedby="button-addon2" />
-                        <button className="btn btn-primary" type="submit" onClick={() => this.props.addFunc(this.state.productName, this.state.price)} id="button-addon2">Add</button>
+                        <input type="text" placeholder="Item" onChange={this.updateProductName} class="form-control" aria-label="Item" />
+                        <button className="btn btn-primary" type="submit" onClick={() => this.props.addItem(this.state.productName, this.state.price)} id="button-addon2">Add</button>
                     </div>
-</div>
-</div>
+                    <div>{this.state.price} €</div>
+                </div >
+
+                <input style={{
+                width: '100%'}} type="range" min={1} max={10} value={this.state.price} onChange={this.updatePrice} />
+            </div >
+
+
             )
         }
 
