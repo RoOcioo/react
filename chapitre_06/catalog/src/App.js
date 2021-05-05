b384227e6242a39c2da955eb0a38b5b5a0009f61
Rocio <arias.roocio@gmail.com>
2021-05-05 11:26:05 +0200

    render problems

--- chapitre_06/catalog/src/App.js
+++ chapitre_06/catalog/src/App.js
@@ -2,21 +2,70 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Router, Route, Link, Switch } from "react-router-dom"
 import Films from './composants/Films.jsx'
-import Filmid from './composants/Filmid.jsx'
+import films from '../../../../backend/api-catalogfilms-react/dataFilms.js';
 
 class App extends React.Component{
+constructor() {
+  super()
+
+  this.state = {
+    filmsArr: []
+  }
+}
+
+componentDidMount() {
+const url = "http://localhost:8000/films/" 
+
+fetch(url)
+    .then(response => response.json())
+    .then(films => {
+        console.log("films", films);
+
+      this.setState= ({
+        filmsArr: films
+      })
+    })
+  }
+
   render() {
     return(
 <Router>
   <div className="container">
-    <h1>Hola</h1>
+  <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+               
+               <a className="navbar-brand" href="/">Films</a>
+               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                   <span class="navbar-toggler-icon"></span>
+               </button>
+
+       </nav>
   
     <Switch>
-    <Route path="/films" Component= {Films} />
-    <Films />
-    <Route path="/films/:id" Component={Filmid} />
-    <Filmid />
+    <Route exact path="/films"></Route>
+render(){
+  return
+  
+  <Films />
 
+<div className="row">
+<div className="col-6 offset-3" >Image : {films.image}</div>
+</div>
+<div className="row">
+<div className= "col-6 offset-3">Id : {films.id}</div>
+</div>
+<div className="row">
+<div className="col-6 offset-3">Film : {films.title}</div>
+</div>
+<div className="row">
+<div className="col-6 offset-3">Director : {films.director}</div>
+</div>
+<div className="row">
+ <div className="col-6 offset-3" >Description : {films.description}</div>
+ </div>
+        
+     
+    
+   
 
 </Switch>
 </div>
