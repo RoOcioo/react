f5a6c46bf2cf095939363a16b8ab5666f13a2536
Rocio <arias.roocio@gmail.com>
2021-05-06 17:21:59 +0200

    errors

--- chapitre_06/moovice/src/App.js
+++ chapitre_06/moovice/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+
+import Home from './components/views/Home';
+import Favorites from './components/views/Favorites';
+import Popular from './components/views/Popular';
+import PopularBattle from './components/views/PopularBattle';
+import Weekly from './components/views/Weekly';
+import WeeklyBattle from './components/views/WeeklyBattle';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
+  render() {
+
+    return (
+      <BrowserRouter>
+      <nav>
+        
+          
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/weekly">Weekly</Link></li>
+              <li><Link to="/weekly-battle">Weekly Battle</Link></li>
+              <li><Link to="/popular">Popular</Link></li>
+              <li><Link to="/popular-battle">Popular Battle</Link></li>
+              <li><Link to="/favorites">Favorites</Link></li>
+            </ul>
+            </nav>
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/weekly" component={Weekly} />
+            <Route exact path="/weekly-battle" component={WeeklyBattle} />
+            <Route exact path="/popular" component={Popular} />
+            <Route exact path="/popular-battle" component={PopularBattle} />
+            <Route exact path="/favorites" component={Favorites} />
+          </Switch> 
+
+        
+      </BrowserRouter>
+    )
+  }
+}
+ 
 export default App;
