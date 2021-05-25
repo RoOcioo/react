54770e7d8143ce67793792d8eafe5241c4b94796
Rocio <arias.roocio@gmail.com>
2021-05-25 16:59:58 +0200

    moovice

--- chapitre_06/moovice/src/views/Favorites.jsx
+++ chapitre_06/moovice/src/views/Favorites.jsx
@@ -1,14 +1,71 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Card from '../components/Card'
+import { getMovieAPI } from '../utils/Api'
 
 
-class Favorites extends React.Component {
-render() {
- return (
-            <div>
-                <h1>Favorites</h1>
-            </div>
-        )
+export class Favorites extends Component {
+
+    state = {
+        movies: [],
+        favIDs: this.getStorage()
+    }
+
+    getStorage() {
+
+        const favorites = JSON.parse(localStorage.getItem("favorites"))
+        console.log("Favorites tyepof favorites", typeof(favorites))
+        return favorites
+    }
+
+    
+    getMovie(id) {
+
+        getMovieAPI(id)
+
+        fetch(`http://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log("Favorites data", data)
+
+                this.setState({
+                    movies: [...this.state.movies, data]
+                })
+
+
+
+            })
+
+    }
+
+    componentDidMount() {
+        
+            this.state.favIDs.map(item => {
+                
+                return this.getMovie(item)
+            })
+        
+    }
+
+    render() {
+        return (
+            <div className="container">
+            <h1 className="text-center">Favorites</h1>
+
+            <div className="row">
+                {
+                    this.state.movies.map(elem => {
+                        return (
+                            <div className="col-6">
+                                <Card {...elem} />
+                            </div>
+                        )
+                    })
+                }
+        </div>
+        </div>
+        );
     }
 }
 
-export default Favorites
+export default Favorites;
+
--- chapitre_06/moovice/src/views/Weekly.jsx
+++ chapitre_06/moovice/src/views/Weekly.jsx
@@ -1,15 +1,60 @@
 import React from 'react';
+import Card from '../components/Card'
+import { getLatestMovies } from '../utils/Api'
+
+const moment = require("moment");
+
+export default class Weekly extends Component {
+
+    state = {
+        movies: []
+    }
+    formatDateToday(dateString){
+
+        const dateToday = moment(dateString).format("YYYY-MM-DD")
+        return dateToday
+    }
+    
+    
+    
+    formatDateLastWeek(dateString){
+
+        const lastWeek =  moment(dateString).subtract(6, 'days').format("YYYY-MM-DD")
+        return lastWeek
+    }
+    
 
 
-class Weekly extends React.Component {
-    render(){
+    componentDidMount() {
+        getLatestMovies()
+            .then(data => {
 
-        return(
-            <div>
-                <h1>Weekly</h1>
+                this.setState({
+                    movies: data.results
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1 className="text-center">Weekly</h1>
+
+                <div className="row">
+                    {
+                        this.state.movies.map(elem => {
+                            return (
+                                <div className="col-6">
+                                    <Card {...elem} />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+
             </div>
-        )
+        );
     }
 }
 
-export default Weekly
