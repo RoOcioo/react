7a75e5e7e84e3fad545a4d8e2ced90ccd3c56d2a
Rocio <arias.roocio@gmail.com>
2021-06-06 00:54:58 +0200

    students corrigé

--- autres/students/src/App.js
+++ autres/students/src/App.js
@@ -1,19 +1,29 @@
 import { useState, useEffect } from 'react'
 import { Switch, Route, BrowserRouter} from 'react-router-dom'
-import List from './components/List'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
+import List from './components/List'
+import AddStudent from './components/AddStudent'
+import studentAdded from './views/StudentAdded'
+
 export default function App() {
+  const [StudentAdded, setStudentAdded] = useState("");
+
   return (
     <BrowserRouter>
-    <div>
+    <div className= "container">
       <h1>Students</h1>
 
       <Switch>
         <Route path="/" exact>
+          <div className="row">
           <List />
+          <AddStudent changeStudentAdded = {(name) => setStudentAdded(name)} />
+          </div>
         </Route>
-        <Route path= "studentAdded" exact>
+        <Route path= "/studentAdded/" >
+          <StudentAdded name={studentAdded} />
         </Route>
       </Switch>
       </div>
