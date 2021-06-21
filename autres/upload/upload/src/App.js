7e6234c6dcb7a11381e0b9a5d5e8dfcb0c16ea04
Rocio <arias.roocio@gmail.com>
2021-06-21 12:22:02 +0200

    upload front

---
+++ autres/upload/upload/src/App.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react'
+import { Switch, Route, BrowserRouter, Link } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import AddUser from './views/AddUser'
+import ListUser from './views/ListUser'
+
+function App() {
+  
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <nav>
+          <ul>
+            <li><Link to="/">Liste d'users</Link></li>
+            <li><Link to="/add-user">Ajouter un nouvel user</Link></li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" >
+            <ListUser/>
+          </Route>
+
+          <Route exact path="/add-user">
+            <AddUser />
+          </Route>
+
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
